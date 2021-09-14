ca674f86798c8fb1f5e8adf6c5b851a5bcce8a06
SerjMartin <serjmartin@mail.com>
2021-09-14 18:11:09 +0000

    Add class ex.

--- arrow-functions.js
+++ arrow-functions.js
@@ -44,3 +44,41 @@ const returnMultipleLines = () => (
     <p>`
 )
 console.log(returnMultipleLines());
+
+// class ex.
+let students = [
+    {
+        name: 'John',
+        subjects: ['maths', 'english', 'cad'],
+        teacher: {maths: 'Harry', english: 'Joan', cad: 'Paul'},
+        results: {maths: 90, english: 75, cad: 87},
+    },
+    {
+        name: 'Emily',
+        subjects: ['science', 'english', 'art'],
+        teacher: {science: 'Iris', english: 'Joan', art: 'Simon'},
+        results: {science: 93, maths: 95, english: 80, art: 95},
+    },
+    {
+        name: 'Adam',
+        subjects: ['science', 'maths', 'art'],
+        teacher: {science: 'Iris', maths: 'Harry', art: 'Simon'},
+        results: {science: 63, maths: 79, art: 95},
+    }
+    
+   
+];
+const averagePoints = (arr, subject) => {
+   let allPoints = 0;
+    let sum = 0;
+    for(let itm of arr) {
+      if(subject in itm.results) {
+        allPoints += itm.results[subject];
+        sum+=1;
+      }
+    }
+    return allPoints / sum;
+};
+
+let result = averagePoints(students, 'english');
+console.log(result);
