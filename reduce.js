a389c36e1a706c30a54b17cc4a97dca00e95e546
SerjMartin <serjmartin@mail.com>
2021-09-19 19:18:08 +0000

    Add experienceByProfession fnction

--- reduce.js
+++ reduce.js
@@ -5,6 +5,17 @@
 
 
 // Summing an array of numbers:
+const nums = [0, 1, 2, 3, 4];
+let sum = nums.reduce((acc, curr) => acc + curr, 0);  // --- or -- (0 is the number were you whant to start from)
+// let sum = nums.reduce((acc, curr) => {
+//     console.log(
+//         "Acumulator:", acc,
+//         "Curent Value:", curr,
+//         "Total:", acc + curr
+//     );
+//      return acc + curr;
+// }, x);        // x is a number were you whant to start from 0 + x 
+console.log(sum);
 
 
 const teamMembers = [
@@ -27,10 +38,28 @@ const teamMembers = [
       name: 'Kelly',
       profession: 'Designer',
       yrsExperience: 3
+    },
+    {
+      name: 'Mark',
+      profession: 'Manager',
+      yrsExperience: 10
     }
   ];
   
   // Totaling a specific object property
+  let totalExperience = teamMembers.reduce((acc, curr) => acc + curr.yrsExperience, 0);
+  console.log(totalExperience);
   
   
   // Grouping by a property, and totaling it too
+  let experienceByProfession = teamMembers.reduce((acc, curr) => {
+    let key = curr.profession;
+    if(!acc[key]) {
+      acc[key] = curr.yrsExperience;
+    } else {
+      acc[key] += curr.yrsExperience;
+    }
+    return acc;
+  }, {});
+
+  console.log(experienceByProfession);
